9a195257e935e2491ef43851acdf8f77ed8ee03a
Marek Marecki <triviuss@gmail.com>
2012-10-15 17:23:27 +0200

    Refresh

--- test.py
+++ test.py
@@ -11,67 +11,61 @@ print( pyproperties.__version__ )
 
 foo = pyproperties.Properties( "./data/properties/foo.properties" )
 bar = pyproperties.Properties( "./data/properties/bar.properties" )
+
 foo.complete( bar )
-foo.merge( bar )
-
-foo_parsed = {  "message.0":"Apple Jack.",
-                "message.1":"Arr... Hello, John the Average!",
-                "name.0":"John the Average",
-                "name.1":"Jack"}
-
-foo_names = ["message.0", "message.1", "name.0", "name.1"]
-
-
-class ParseTest( unittest.TestCase ):
-    def test_parse(self):
-        """
-        parse should return properly parsed dict.
-        """
-        self.assertEqual( foo_parsed, foo.parse() )
-
-
-class MergeTest( unittest.TestCase ):
-    def test_merge(self):
-        """
-        merge should replace properties values, but not add any new value. 
-        """
-        #  TODO
-
 
 class GetterTest( unittest.TestCase ):
     def test_get_parsed(self):
-        """
-        should return properly parsed line containg reference
-        """
-        self.assertEqual( "Apple Jack.", foo.get( "message.0", True ) )
+        self.assertEqual( "Yellow Jack.", foo.get( "message.0", True ) )
 
 
     def test_get_not_parsed(self):
-        """
-        should return a not-parsed line
-        """
-        self.assertEqual( "Apple $(name.1).", foo.get( "message.0", False ) )
+        self.assertEqual( "Yellow $(name.1).", foo.get( "message.0" ) )
 
 
     def test_gets_parsed(self):
-        """
-        gets should return a dict of properly parsed values
-        """
-        self.assertEqual( {"message.0":"Apple Jack.", "message.1":"Arr... Hello, John the Average!"}, foo.gets( "message.*", True ) )
+        self.assertEqual( {"message.0":"Yellow Jack.", "message.1":"Arr... Hello, William!"}, foo.gets( "message.*", True ) )
 
 
     def test_gets_not_parsed(self):
-        """
-        gets should return a dict of not-parsed values
-        """
-        self.assertEqual( {"message.0":"Apple $(name.1).", "message.1":"Arr... Hello, $(name.0)!"}, foo.gets( "message.*" ) )
+        self.assertEqual( {"message.0":"Yellow $(name.1).", "message.1":"Arr... Hello, $(name.0)!"}, foo.gets( "message.*" ) )
 
 
-    def test_getnames(self):
-        """
-        getnames should return list of all names in dict.
-        """
-        self.assertEqual( foo_names, foo.getnames() )
+class SetterTest( unittest.TestCase ):
+    def test_set(self):
+        foo.set( "message.0", "Yellow $(name.0)." )
+        self.assertEqual( "Yellow $(name.0).", foo.get( "message.0" ) )
+
+
+    def test_sets(self):
+        foo.sets( "message.*", "Yellow $(name.0)." )
+        self.assertEqual( {"message.0":"Yellow $(name.0).", "message.1":"Yellow $(name.0)."}, foo.gets( "message.*" ) )
+
+
+class RemoverTest( unittest.TestCase ):
+    def test_remove(self):
+        foo.remove( "message.0" )
+        self.assertRaises( KeyError, foo.get, "message.0" )
+        foo.set( "message.0", "Yellow $(name.0)." )
+
+
+    def test_removes(self):
+        foo.removes( "message.*" )
+        self.assertEqual( {}, foo.gets( "message.*" ) )
+        foo.set( "message.0", "Yellow $(name.0)." )
+        foo.set( "message.1", "Yellow $(name.0)." )
+
+
+class PopperTest( unittest.TestCase ):
+    def test_pop(self):
+        self.assertEqual( "Yellow $(name.1).", foo.pop( "message.0" ) )
+        foo.set( "message.0", "Yellow $(name.1)." )
+
+
+    def test_pops(self):
+        self.assertEqual( {"message.0":"Yellow $(name.1).", "message.1":"Arr... Hello, $(name.0)!"}, foo.pops( "message.*" ) )
+        foo.set( "message.0", "Yellow $(name.0)." )
+        foo.set( "message.1", "Yellow $(name.0)." )
 
 
 if __name__ == '__main__':
